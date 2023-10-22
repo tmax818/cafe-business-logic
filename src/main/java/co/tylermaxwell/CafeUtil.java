27fc02e1e45ef50741ea79e98720c0ecc2f78cec
tmax818 <tylermaxwell661@gmail.com>
2023-10-22 16:42:02 -0700

    update tests

--- src/main/java/co/tylermaxwell/CafeUtil.java
+++ src/main/java/co/tylermaxwell/CafeUtil.java
@@ -6,11 +6,25 @@ public class CafeUtil {
 
 
     public int getStreakGoal(){
-        return 0;
+        int sum = 0;
+        for(int i = 0; i <= 10; i++){
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int getStreakGoal(int numWeeks){
+        int sum = 0;
+        for(int i = 0; i <= numWeeks; i++){
+            sum += i;
+        }
+        return sum;
     }
 
     public double getOrderTotal(double[] prices){
-        return 0.0;
+        double sum = 0;
+        for(double price : prices) sum += price;
+        return sum;
     }
 
     public void displayMenu(ArrayList<String> menuItems){
--- src/test/java/co/tylermaxwell/CafeUtilTest.java
+++ src/test/java/co/tylermaxwell/CafeUtilTest.java
@@ -3,6 +3,8 @@ package co.tylermaxwell;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CafeUtilTest {
@@ -20,11 +22,28 @@ class CafeUtilTest {
     }
 
     @Test
+    void getStreakGoalTestWithParameter() {
+        assertEquals(cu.getStreakGoal(10), 55);
+    }
+
+    @Test
     void getOrderTotalTest() {
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        assertEquals(cu.getOrderTotal(lineItems), 13.5);
     }
 
     @Test
     void displayMenuTest() {
+        String message = """
+                0 drip coffee
+                1 cappuccino
+                2 latte
+                3 mocha""";
+         ArrayList<String> menu = new ArrayList<String>();
+         menu.add("drip coffee");
+         menu.add("cappuccino");
+         menu.add("latte");
+         menu.add("mocha");
     }
 
     @Test
